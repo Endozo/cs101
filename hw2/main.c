fcd38132de5b102298eb3bd8808eb84b155fd5df
B1029007 <williamw712kd@gmail.com>
2022-03-08 00:48:18 +0800

    second upload

--- hw2/main.c
+++ hw2/main.c
@@ -3,9 +3,11 @@
 #include<time.h>
 
 int main() {
+    int n;
     int set;
     int lotto[5][6];
     int alotto[5][6];
+    int num[69][5] = {0};
     int min = 1;
     int Max = 69;
     int sMax = 10;
@@ -21,9 +23,14 @@ int main() {
 
     for (int i = 0; i < set; i++) {
         for (int j = 0; j < 5; j++) {
-            lotto[i][j] = rand() % (Max - min + 1) - min;
+            n = rand() % (Max - min + 1) + min;
+            while (num[n - 1][j] == 1) {
+                n = rand() % (Max - min + 1) + min;
+            }
+            lotto[i][j] = n;
+            num[n - 1][j] = 1;
         }
-        lotto[i][5] = rand() % (sMax - min + 1) - min;
+        lotto[i][5] = rand() % (sMax - min + 1) + min;
     }
 
     for (int i = 0; i < set; i++) {
@@ -51,13 +58,16 @@ int main() {
     for (int i = 0; i < set; i++) {
         fprintf(fp, "[%d]: ", i + 1);
         for (int j = 0; j < 6; j++) {
+            if (alotto[i][j] < 10) {
+                fprintf(fp, "0");
+            }
             fprintf(fp, "%d ", alotto[i][j]);
         }
         fprintf(fp, "\n");
     }
     while (set < 5) {
         set += 1;
-        fprintf(fp, "[%d]: -- -- -- -- -- -\n", set);
+        fprintf(fp, "[%d]: -- -- -- -- -- --\n", set);
     }
     
     fclose(fp);
